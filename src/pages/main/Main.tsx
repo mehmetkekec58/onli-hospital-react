1e81de0bd95b5f0c856f5f94abeef4b016ad5033
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-28 16:04:52 +0300

    added grid for main

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -3,18 +3,26 @@ import Navi from '../../layouts/navi/Navi'
 import { Route, Routes } from 'react-router-dom';
 import Home from '../home/Home';
 import Search from '../search/Search';
+import Drawer from '../../layouts/drawer/Drawer';
+import Profile from '../profile/Profile';
+import "./Main.css";
 
 const Main = () => {
   return (
     <div>
       <Navi />
 
-      <Routes>
-        <Route path='' element={<Home />} />
-        <Route path='search' element={<Search />} />
-      </Routes>
-
-      Main</div>
+      <div className='grid-container'>
+        <div className='grid-item'> <Drawer /></div>
+        <div className='grid-item'>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/profile' element={<Profile />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   )
 }
 

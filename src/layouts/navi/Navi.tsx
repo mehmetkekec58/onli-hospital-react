db7a7a0fc00054ef5b3e6219f369bb2e606601a7
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-28 16:04:23 +0300

    updated witdth, height and color for avatar

--- src/layouts/navi/Navi.tsx
+++ src/layouts/navi/Navi.tsx
@@ -13,7 +13,7 @@ const Navi = () => {
         <div className='navi-icon-for-drawer-open'><MenuIcon sx={{ color: 'white' }} /></div>
         <div className='brand-name-for-navi'>{containTexts.brandName}</div>
         <div className='navi-input'><Input /></div>
-        <div className='navi-avatar'><Avatar alt='Mehmet' sx={{ backgroundColor: 'yellow', color: 'black', width: 30, height: 30 }}>M</Avatar></div>
+        <div className='navi-avatar'><Avatar alt='Mehmet' sx={{ backgroundColor: '#ffd740', color: 'black', width: 35, height: 35 }}>M</Avatar></div>
       </div>
 
     </div>

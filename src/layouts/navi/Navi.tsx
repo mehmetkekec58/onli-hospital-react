14fb27b82f385576f19af6e469f02e8caa86eb34
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-28 15:07:42 +0300

    updated navi

--- src/layouts/navi/Navi.tsx
+++ src/layouts/navi/Navi.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import "./Navi.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import { containTexts } from '../../contains/containTexts';
+import Input from '../input/Input';
+import Avatar from '@mui/material/Avatar/Avatar';
 
 const Navi = () => {
-  const brandName:string = "Onli Hospital";
+
   return (
     <div className='navi-component'>
       <div className='navi-items-general-div'>
-      <div className='brand-name-for-navi'>{brandName}</div>
+        <div className='navi-icon-for-drawer-open'><MenuIcon sx={{ color: 'white' }} /></div>
+        <div className='brand-name-for-navi'>{containTexts.brandName}</div>
+        <div className='navi-input'><Input /></div>
+        <div className='navi-avatar'><Avatar alt='Mehmet' sx={{ backgroundColor: 'yellow', color: 'black', width: 30, height: 30 }}>M</Avatar></div>
       </div>
- 
+
     </div>
   )
 }

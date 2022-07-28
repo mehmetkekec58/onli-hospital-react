d199984a89159cab9c22c7b0ae88017cee72eb97
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-28 22:54:44 +0300

    activited search botton and  added useNavigate

--- src/layouts/input/Input.tsx
+++ src/layouts/input/Input.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Input.css"
 import SearchIcon from '@mui/icons-material/Search';
 import { containTexts } from '../../contains/containTexts';
+import { useNavigate } from 'react-router-dom';
+
+const containSearchUrl: string = "search?q=";
+const enter: string = "Enter";
+
 const Input = () => {
-  return (
-<form className='search-input-form'>
-    <input className="search-input" placeholder={containTexts.search} type="text" />
-    <button className='search-button' type='submit'><SearchIcon  className='search-icon'/></button>
-</form>
-  )
+
+    let navigate = useNavigate();
+
+    const [inputText, setInputText] = useState<string | null>(null)
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
+        if (event.key === enter) {
+            goSearchPage()
+        }
+    }
+    const handleSearchButton = () => {
+        goSearchPage()
+    }
+
+    function goSearchPage() {
+        if (!inputTextNullOrEmpty()) {
+            navigate(`${containSearchUrl}${inputText}`)
+        }
+    }
+
+    function inputTextNullOrEmpty(): boolean {
+        return inputText === "" || inputText === null;
+    }
+    return (
+        <div className='search-input-form'>
+            <input onChange={(e) => setInputText(e.target.value)} className="search-input" placeholder={containTexts.search} type="text" onKeyDown={handleKeyDown} />
+            <button  disabled={inputTextNullOrEmpty()? true : false} onClick={handleSearchButton} className='search-button' ><SearchIcon className='search-icon' /></button>
+        </div>
+    )
 }
 
 export default Input

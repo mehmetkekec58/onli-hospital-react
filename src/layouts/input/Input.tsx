bc36a76f123d53ad736231075099f17bf2e8a54e
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-28 16:01:32 +0300

    added placeholder for search input

--- src/layouts/input/Input.tsx
+++ src/layouts/input/Input.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./Input.css"
 import SearchIcon from '@mui/icons-material/Search';
-
+import { containTexts } from '../../contains/containTexts';
 const Input = () => {
   return (
 <form className='search-input-form'>
-    <input className="search-input" type="text" />
+    <input className="search-input" placeholder={containTexts.search} type="text" />
     <button className='search-button' type='submit'><SearchIcon  className='search-icon'/></button>
 </form>
   )

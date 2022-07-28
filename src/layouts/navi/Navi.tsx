04561998bc9c92910f42dd625fa3e837b75c3eff
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-28 22:58:19 +0300

    Added drawer opening and closing for menu icon

--- src/layouts/navi/Navi.tsx
+++ src/layouts/navi/Navi.tsx
@@ -4,16 +4,27 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { containTexts } from '../../contains/containTexts';
 import Input from '../input/Input';
 import Avatar from '@mui/material/Avatar/Avatar';
+import { Link } from 'react-router-dom';
 
-const Navi = () => {
+interface Props {
+  openDrawer: boolean;
+  setOpenDrawer: React.Dispatch<React.SetStateAction<boolean>>
+}
 
+const Navi: React.FC<Props> = ({ openDrawer, setOpenDrawer }: Props) => {
+  const profilePhotoUrl = "https://pbs.twimg.com/profile_images/1523976377074163713/hRUFPi6z_400x400.jpg"
+
+  const handleOpenOrCloseDrawer = () => {
+    setOpenDrawer(!openDrawer)
+  }
   return (
     <div className='navi-component'>
       <div className='navi-items-general-div'>
-        <div className='navi-icon-for-drawer-open'><MenuIcon sx={{ color: 'white' }} /></div>
-        <div className='brand-name-for-navi'>{containTexts.brandName}</div>
+        <div onClick={handleOpenOrCloseDrawer} className='navi-icon-for-drawer-open'><MenuIcon sx={{ color: 'white' }} /></div>
+        <div className='brand-name-for-navi'>  <Link style={{ textDecoration: 'none', color: 'white' }} to="/">{containTexts.brandName}</Link></div>
         <div className='navi-input'><Input /></div>
-        <div className='navi-avatar'><Avatar alt='Mehmet' sx={{ backgroundColor: '#ffd740', color: 'black', width: 35, height: 35 }}>M</Avatar></div>
+        <div className='navi-avatar'><Avatar   alt='Mehmet' src={profilePhotoUrl} sx={{ backgroundColor: '#ffd740', border: '2px #ffd740 solid', color: '#ffd740', width: 50, height: 50 }}>M</Avatar></div>
+       
       </div>
 
     </div>

a5520df02a34558204df141bbbd3122d8a5e2890
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-29 19:42:30 +0300

    updated avatar for navi layout

--- src/layouts/navi/Navi.tsx
+++ src/layouts/navi/Navi.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 const Navi: React.FC<Props> = ({ openDrawer, setOpenDrawer }: Props) => {
   const profilePhotoUrl = "https://pbs.twimg.com/profile_images/1523976377074163713/hRUFPi6z_400x400.jpg"
-
+  const userFullName:string = "Mehmet Kekeç"
   const handleOpenOrCloseDrawer = () => {
     setOpenDrawer(!openDrawer)
   }
@@ -23,8 +23,8 @@ const Navi: React.FC<Props> = ({ openDrawer, setOpenDrawer }: Props) => {
         <div onClick={handleOpenOrCloseDrawer} className='navi-icon-for-drawer-open'><MenuIcon sx={{ color: 'white' }} /></div>
         <div className='brand-name-for-navi'>  <Link style={{ textDecoration: 'none', color: 'white' }} to="/">{containTexts.brandName}</Link></div>
         <div className='navi-input'><Input /></div>
-        <div className='navi-avatar'><Avatar   alt='Mehmet' src={profilePhotoUrl} sx={{ backgroundColor: '#ffd740', border: '2px #ffd740 solid', color: '#ffd740', width: 50, height: 50 }}>M</Avatar></div>
-       
+        <div className='navi-avatar'><Avatar alt={userFullName} src={profilePhotoUrl} sx={{ backgroundColor: '#ffd740', border: '2px black solid', color: 'black', fontWeight: 'bold', width: 45, height: 45 }} /></div>
+
       </div>
 
     </div>

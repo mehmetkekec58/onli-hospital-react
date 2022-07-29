07c15244776ea3a950baf709eace3be0756577e2
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-29 19:40:20 +0300

    added article model

---
+++ src/models/articleModel.ts
@@ -0,0 +1,11 @@
+export default interface ArticleModel {
+    articleId:number;
+    thumbnailUrl:string;
+    username:string;
+    profilePhotoUrl:string;
+    firstName:string;
+    lastName:string;
+    title:string;
+    text:string;
+    tag:string[];
+}

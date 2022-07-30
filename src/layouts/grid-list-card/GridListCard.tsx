97d8ca6d949d42a968232f302d7c7d6f43c6997e
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-30 15:28:10 +0300

    added grid list article card and screen size

---
+++ src/layouts/grid-list-card/GridListCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ArticleModel from '../../models/articleModel'
+import ArticleCard from '../article-card/ArticleCard'
+import "./GridListCard.css"
+
+interface Props{
+    articles:ArticleModel[]
+}
+
+const GridListCard : React.FC<Props> = ({articles}:Props) => {
+  return (
+    <div className="grid-list-card-grid-container">
+            {articles.map((article) => (
+              <div key={article.articleId} className="grid-list-card-grid-item"> <ArticleCard article={article} /></div>
+            ))}
+          </div>
+  )
+}
+
+export default GridListCard

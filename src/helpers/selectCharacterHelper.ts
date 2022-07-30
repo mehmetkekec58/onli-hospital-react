028ba3b8171375193c0b6894d9f62e2b30b722b8
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-30 15:23:19 +0300

    the number of selected characters has been changed

--- src/helpers/selectCharacterHelper.ts
+++ src/helpers/selectCharacterHelper.ts
@@ -1,5 +1,5 @@
 export default function selectCharacterHelper(text: string): string {
-    let number: number = 150
+    let number: number = 120
     return text.length > number ? `${substringText(text, number)}...` : text
 }
 function substringText(text: string, number: number): string {

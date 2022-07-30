1ebddb742a5b12d24c5017a864d550e4b617f275
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-30 15:31:13 +0300

    added doctor model

---
+++ src/models/doctorModel.ts
@@ -0,0 +1,9 @@
+export default interface DoctorModel{
+    userId:number;
+    firstName:string;
+    lastName:string;
+    profilePhotoUrl:string;
+    username:string;
+    branch:string;
+    about:string;
+}

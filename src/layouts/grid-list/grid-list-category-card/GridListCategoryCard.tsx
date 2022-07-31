d494125a045f924724948d5c7a55c42ac88f1841
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-31 11:34:02 +0300

    added grid list category card

---
+++ src/layouts/grid-list/grid-list-category-card/GridListCategoryCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CategoryModel from '../../../models/categoryModel';
+import CategoryCard from '../../card/category-card/CategoryCard';
+import './GridListCategoryCard.css';
+
+interface Props{
+    categories:CategoryModel[]
+}
+
+const GridListCategoryCard:React.FC<Props> = ({categories}:Props) => {
+  return (
+    <div className="grid-list-category-card-grid-container">
+            {categories.map((category,index) => (
+              <div key={index} className="grid-list-category-card-grid-item"> <CategoryCard category={category} /></div>
+            ))}
+          </div>
+  )
+}
+
+export default GridListCategoryCard

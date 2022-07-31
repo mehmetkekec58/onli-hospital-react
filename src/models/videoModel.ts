d23854da5c1f609f702ae3a82ee479c61a90bba8
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-31 11:35:09 +0300

    added video Model

---
+++ src/models/videoModel.ts
@@ -0,0 +1,13 @@
+export default interface VideoModel{
+ videoId:number;
+ videoUrl:string;
+ thumbnailUrl:string;
+ title:string;
+ explanation:string;
+ tags:string[];
+ date:Date;
+ username:string;
+ firstName:string;
+ lastName:string;
+ profilePhotoUrl:string;
+}

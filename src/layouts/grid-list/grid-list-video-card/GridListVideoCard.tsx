e0c91b2723c9f85c2021d1e956c7fe31a7a5c3ed
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-31 11:34:24 +0300

    added grid list video card

---
+++ src/layouts/grid-list/grid-list-video-card/GridListVideoCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import VideoModel from '../../../models/videoModel';
+import VideoCard from '../../card/video-card/VideoCard';
+import './GridListVideoCard.css';
+
+interface Props{
+    videos : VideoModel[]
+}
+
+const GridListVideoCard:React.FC<Props> = ({videos}:Props) => {
+  return (
+    <div className="grid-list-video-card-grid-container">
+    {videos.map((video,index) => (
+      <div key={index} className="grid-list-video-card-grid-item"> <VideoCard video={video} /></div>
+    ))}
+  </div>
+  )
+}
+
+export default GridListVideoCard

f4403efbec86dcb18783ef5671e111691a1e707e
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-31 11:30:14 +0300

    added category card

--- src/layouts/card/category-card/CategoryCard.tsx
+++ src/layouts/card/category-card/CategoryCard.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import CategoryModel from '../../../models/categoryModel'
 import "./CategoryCard.css"
 
-const CategoryCard = () => {
+interface Props{
+  category:CategoryModel 
+}
+
+const CategoryCard:React.FC<Props> = ({category}:Props) => {
   return (
-    <div>CategoryCard</div>
+    <div className='category-card'>
+      <img alt={category.categoryName} className='category-card-photo' src={category.categoryPhotoUrl} />
+      <h4 className='category-card-name'>{category.categoryName} </h4>
+    </div>
   )
 }
 

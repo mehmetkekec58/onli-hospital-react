ae48fab04d83c9e666322bd5f498627007643cba
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-01 19:56:37 +0300

    added new contain texts

--- src/contains/containTexts.ts
+++ src/contains/containTexts.ts
@@ -1,4 +1,35 @@
 export const containTexts = {
     brandName:"Onli Hospital",
-    search:"Ara...",
+
+    search:"Ara",
+
+    register:"Kayıt Ol",
+    login:"Giriş Yap",
+    alreadyHaveAnAccountText:"Hesabınız var mı? Hemen giriş yapın",
+    rememberMe:"Beni hatırla",
+    forgetMyPassword:"Şifremi unuttum",
+    dontHaveAnAccountText:"Hesabınız yok mu? Hemen kaydolun",
+    
+    mostReadArticles:"En Çok Okunanlar:",
+    popularDoctors:"Popüler Doktorlar:",
+    categories:"Kategoriler:",
+    recommendedVideos:"Önerilen Videolar:",
+
+    readMore:"Devamını Oku",
+
+    firstName:"Adı",
+    lastName:"Soyadı",
+    username:"Kullanıcı Adı",
+    email:"E-posta",
+    password:"Şifre",
+
+    homePage:"Ana Sayfa",
+    purse:"Cüzdan",
+    editProfile:"Profili Düzenle",
+    history:"Geçmiş",
+    readingList:"Okuma Listesi",
+    settings:"Ayarlar",
+    about: "Hakkında",
+    logout:"Çıkış Yap",
+    loadMoney:"Para Yükle",
 };

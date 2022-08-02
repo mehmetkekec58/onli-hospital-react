09c10dd40b770f0122906256f64125009626baf8
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-02 15:08:42 +0300

    input bug fix and some regulations

--- src/layouts/input/Input.tsx
+++ src/layouts/input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./Input.css"
 import SearchIcon from '@mui/icons-material/Search';
 import { containTexts } from '../../contains/containTexts';
@@ -9,24 +9,18 @@ const containSearchUrl: string = "search?q=";
 const enter: string = "Enter";
 
 const Input = () => {
-    const searchList: string[] = ["kanser nedir", "yaz mevsimi hastalıkları", "Grip nasıl geçer", "renk körü", "yorgunluk"];
+
 
     let navigate = useNavigate();
-
+    const [searchList] = useState<string[]>(["kanser nedir", "yaz mevsimi hastalıkları", "Grip nasıl geçer", "renk körü", "yorgunluk"])
+    const inputRef = useRef<HTMLInputElement>(null);
     const [inputText, setInputText] = useState<string>("")
     const [inputOnFocus, setInputOnFocus] = useState<boolean>(false);
-    const [searchListItems, setsearchListItems] = useState<string[]>(searchList)
+    const [searchListItems, setsearchListItems] = useState<(string)[]>(searchList)
 
     useEffect(() => {
         if (inputText !== "") {
-            let array: string[] = []
-            searchList.map(item => {
-                if (item.toLocaleLowerCase().includes(inputText.toLocaleLowerCase())) {
-                    array[array.length] = item
-                }
-            })
-            setsearchListItems(array)
-
+            setsearchListItems(searchList.filter((item) => (item.toLocaleLowerCase().includes(inputText.toLocaleLowerCase())) && item))
         } else {
             setsearchListItems(searchList);
         }
@@ -34,27 +28,28 @@ const Input = () => {
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
         if (event.key === enter) {
+            inputRef.current?.blur();
             goSearchPage()
         }
     }
     const handleSearchButton = () => {
         goSearchPage()
     }
-    const handleActiveSearchItems = () => {
-        setTimeout(inputFocusChange, 100)
+    const handleActiveOrPassiveSearchItems = () => {
+        setTimeout(inputFocusChangeValue, 100)
 
     }
 
-    const handleActiveSearchItemsClick = (letters: string) => {
+    const handleSearchItemsClick = (letters: string) => {
         setInputText(letters)
-        inputFocusChange()
+        inputFocusChangeValue()
     }
     function goSearchPage() {
         if (!inputTextNullOrEmpty()) {
             navigate(`${containSearchUrl}${inputText}`)
         }
     }
-    function inputFocusChange() {
+    function inputFocusChangeValue() {
         setInputOnFocus(!inputOnFocus)
     }
 
@@ -63,12 +58,14 @@ const Input = () => {
     }
     return (
         <div className='search-input-form'>
-            <input onBlur={handleActiveSearchItems} onFocus={handleActiveSearchItems} value={inputText} onChange={(e) => setInputText(e.target.value)} className="search-input" placeholder={containTexts.search} type="text" onKeyDown={handleKeyDown} />
-            <ul style={{ ...(!inputOnFocus && { display: 'none' }) }} className='search-input-recommended'>
-                {searchListItems.map((word, index) => (
-                    <Link style={{ textDecoration: 'none', color: 'black' }} onClick={(e) => handleActiveSearchItemsClick(word)} key={index} to={`search?q=${word}`}><li className='search-input-recommended-item'><HistoryOutlinedIcon style={{marginRight:'5px'}}/>{word}</li></Link>
-                ))}
-            </ul>
+            <input ref={inputRef} onBlur={handleActiveOrPassiveSearchItems} onFocus={handleActiveOrPassiveSearchItems} value={inputText} onChange={(e) => setInputText(e.target.value)} className="search-input" placeholder={containTexts.search} type="text" onKeyDown={handleKeyDown} />
+            {searchListItems.length !== 0 &&
+                (<ul style={{ ...(!inputOnFocus && { display: 'none' }) }} className='search-input-recommended'>
+                    {searchListItems.map((word, index) => (
+                        <Link style={{ textDecoration: 'none', color: 'black' }} onClick={(e) => handleSearchItemsClick(word)} key={index} to={`search?q=${word}`}><li className='search-input-recommended-item'><HistoryOutlinedIcon style={{ marginRight: '5px' }} />{word}</li></Link>
+                    ))}
+                </ul>)
+            }
             <button disabled={inputTextNullOrEmpty() ? true : false} onClick={handleSearchButton} className='search-button' ><SearchIcon className='search-icon' /></button>
         </div>
     )

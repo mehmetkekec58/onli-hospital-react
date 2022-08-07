a11868ec1a152d2ef06f6694f465195e441bdc57
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-07 18:35:39 +0300

    added post model

---
+++ src/models/postModel.ts
@@ -0,0 +1,11 @@
+export default interface PostModel{
+    postId:number;
+    thumbnailUrl:string;
+    username:string;
+    profilePhotoUrl:string;
+    firstName:string;
+    lastName:string;
+    text:string;
+    date:Date;
+
+}

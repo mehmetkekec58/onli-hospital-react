056614d37192ef1ecfae011638359755ed327b95
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-09 13:52:28 +0300

    added localStorageService.test

---
+++ src/services/localStorageService.test.tsx
@@ -0,0 +1,17 @@
+import { setLocalStorage, getLocalStorage,deleteLocalStorage } from "./localStorageService";
+
+describe("localStorage service", () => {
+    it("setLocalStorage and getLocalStorage test", () => {
+        setLocalStorage("hello", {test:"merhaba",deneme:"deneme"})
+        const get = getLocalStorage("hello")
+        expect(get).toStrictEqual({test:"merhaba", deneme:"deneme"})
+       
+     
+    });
+
+    it("delete localStorage test", ()=>{
+        deleteLocalStorage("hello");
+        const get = getLocalStorage("hello")
+        expect(get).toStrictEqual(null)
+    })
+})

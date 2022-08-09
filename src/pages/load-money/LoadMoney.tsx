cd22c586be60e041cfa3103a8ae71603afe4ff83
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-09 13:49:31 +0300

    added load money

---
+++ src/pages/load-money/LoadMoney.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import GridListLoadMoneyCard from '../../layouts/grid-list/grid-list-load-money-card/GridListLoadMoneyCard'
+import CreditPackageModel from '../../models/creditPackageModel'
+import './LoadMoney.css'
+
+const creditPackages: CreditPackageModel[] = [
+{
+    creditPackageId:0,
+    creditPackageName:"En düşük paket",
+    price:0.25,
+    discount:0,
+    creditAmount:1,
+},
+{
+    creditPackageId:2,
+    creditPackageName:"Orta paket paket",
+    price:5.0,
+    discount:0,
+    creditAmount:20,
+},
+{
+    creditPackageId:1,
+    creditPackageName:"Al doya doya harca",
+    price:10.0,
+    discount:0,
+    creditAmount:40,
+},
+
+{
+    creditPackageId:3,
+    creditPackageName:"Güç paketi paket",
+    price:100.0,
+    discount:0,
+    creditAmount:400,
+},
+]
+
+const LoadMoney = () => {
+
+    return (
+        <div className='load-money-container'>
+           <GridListLoadMoneyCard creditPackages={creditPackages}/> 
+        </div>
+    )
+}
+
+export default LoadMoney

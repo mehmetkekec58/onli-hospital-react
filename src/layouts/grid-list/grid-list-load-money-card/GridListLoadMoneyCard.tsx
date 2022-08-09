c208019a362013237e54923e78c2ce376f264804
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-09 13:54:58 +0300

    added GridListLoadMoneyCard

---
+++ src/layouts/grid-list/grid-list-load-money-card/GridListLoadMoneyCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import CreditPackageModel from '../../../models/creditPackageModel';
+import LoadMoneyCard from '../../card/load-money-card/LoadMoneyCard';
+import './GridListLoadMoneyCard.css';
+
+interface Props{
+    creditPackages:CreditPackageModel[]
+}
+const GridListLoadMoneyCard = ({creditPackages}:Props) => {
+  return (
+    <div className="grid-list-load-money-card-grid-container">
+    {creditPackages.map((creditPackage,index) => (
+      <div key={index} className="grid-list-load-money-card-grid-item"> <LoadMoneyCard creditPackage={creditPackage} /></div>
+    ))}
+  </div>
+  )
+}
+
+export default GridListLoadMoneyCard

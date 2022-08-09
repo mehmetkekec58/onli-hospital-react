2f6060397844ff4719f4f22d6569363a9235cdc0
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-09 13:43:04 +0300

    added new menus

--- src/layouts/drawer/Drawer.tsx
+++ src/layouts/drawer/Drawer.tsx
@@ -11,6 +11,7 @@ import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
 import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import PaymentsOutlinedIcon from '@mui/icons-material/PaymentsOutlined';
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 import { NavLink } from 'react-router-dom';
 import { containTexts } from '../../contains/containTexts';
 
@@ -18,18 +19,18 @@ interface Props {
     openDrawer: boolean;
 }
 const Drawer: React.FC<Props> = ({ openDrawer }: Props) => {
-    const activeMenus: boolean[] = [true, false, false, true, true, true, true, true, true, true, true]
-    // const activeMenus: boolean[] = [true, true, true, false, false, false, false, false, false, false, true] // when no login
-    const menuLinks: string[] = ['/', 'login', 'register', 'editprofile', 'payments', 'buycredit', 'history', 'readinglist', 'settings', 'logout', 'about']
-    const menuIcons = [<HomeIcon />, <LoginOutlinedIcon />, <HowToRegOutlinedIcon />, <PersonIcon />, <PaymentsOutlinedIcon />, <CreditCardOutlinedIcon />, <HistoryIcon />, <BookIcon />, <SettingsIcon />, <LogoutIcon />, <InfoOutlinedIcon />]
-    const menus: (string | undefined)[] = [containTexts.homePage, containTexts.login, containTexts.register, containTexts.editProfile, containTexts.payments, containTexts.loadMoney, containTexts.history, containTexts.readingList, containTexts.settings, containTexts.logout, containTexts.about]
+    const activeMenus: boolean[] = [true, false, false, true, true, true, true, true, true, true, true, true]
+    // const activeMenus: boolean[] = [true, true, true, false, false ,false, false, false, false, false, false, true] // when no login
+    const menuLinks: string[] = ['/', 'login', 'register', 'editprofile', 'article/add', 'payments', 'buycredit', 'history', 'readinglist', 'settings', 'logout', 'about']
+    const menuIcons = [<HomeIcon />, <LoginOutlinedIcon />, <HowToRegOutlinedIcon />, <PersonIcon />, <CreateOutlinedIcon />, <PaymentsOutlinedIcon />, <CreditCardOutlinedIcon />, <HistoryIcon />, <BookIcon />, <SettingsIcon />, <LogoutIcon />, <InfoOutlinedIcon />]
+    const menus: (string | undefined)[] = [containTexts.homePage, containTexts.login, containTexts.register, containTexts.editProfile, containTexts.addArticle, containTexts.payments, containTexts.loadMoney, containTexts.history, containTexts.readingList, containTexts.settings, containTexts.logout, containTexts.about]
 
     return (
         <div className='drawer-general-div'>
             {menus.map((menu, index) => (
                 activeMenus[index] &&
-                <NavLink  title={menu} key={index} style={({isActive}) => {return isActive ? {textDecoration: 'none', color: '#6b1e9c' } :{textDecoration: 'none', color: 'black' }}} to={menuLinks[index]}>
-                    <div  className={!openDrawer ? 'drawer-item-container-when-close-drawer' : 'drawer-item-container'}>
+                <NavLink title={menu} key={index} style={({ isActive }) => { return isActive ? { textDecoration: 'none', color: '#6b1e9c' } : { textDecoration: 'none', color: 'black' } }} to={menuLinks[index]}>
+                    <div className={!openDrawer ? 'drawer-item-container-when-close-drawer' : 'drawer-item-container'}>
                         <div className={!openDrawer ? 'drawer-menu-icon-when-close-drawer' : 'drawer-menu-icon'}>{menuIcons[index]}</div>
                         <div style={{ ...(!openDrawer && { display: 'none' }) }} className='drawer-menu-text'>{menu}</div>
                     </div>

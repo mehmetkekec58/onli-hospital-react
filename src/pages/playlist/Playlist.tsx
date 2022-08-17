d9ef8347e91fe2bd92ebc30c24bcf162a5d9a38b
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-17 22:40:58 +0300

    added playlist

---
+++ src/pages/playlist/Playlist.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import GridListVideoCard from '../../layouts/grid-list/grid-list-video-card/GridListVideoCard';
+import VideoModel from '../../models/videoModel';
+import './Playlist.css';
+
+
+const videos:VideoModel[]=[
+    {
+      videoId:0,
+      videoUrl:"http://static.videogular.com/assets/videos/videogular.mp4",
+      thumbnailUrl:"https://im.haberturk.com/2019/02/05/ver1549345599/2341369_810x458.jpg",
+      title:"Kanser Oluşumu",
+      explanation:"Kanser hakkında detaylı animasyon video",
+      date:new Date(2022,8,3),
+      tags:["kanser","kanser oluşumu"],
+      username:"mehmetkekec",
+      firstName:"Mehmet",
+      lastName:"Kekeç",
+      profilePhotoUrl:"https://pbs.twimg.com/profile_images/1523976377074163713/hRUFPi6z_400x400.jpg",
+    },
+    {
+      videoId:2,
+      videoUrl:"http://static.videogular.com/assets/videos/videogular.mp4",
+      thumbnailUrl:"https://im.haberturk.com/2019/02/05/ver1549345599/2341369_810x458.jpg",
+      title:"Kanser Oluşumu",
+      explanation:"Kanser hakkında detaylı animasyon video",
+      date:new Date(2022,8,3),
+      tags:["kanser","kanser oluşumu"],
+      username:"mehmetkekec",
+      firstName:"Mehmet",
+      lastName:"Kekeç",
+      profilePhotoUrl:"https://pbs.twimg.com/profile_images/1523976377074163713/hRUFPi6z_400x400.jpg",
+    },
+    {
+      videoId:3,
+      videoUrl:"http://static.videogular.com/assets/videos/videogular.mp4",
+      thumbnailUrl:"https://im.haberturk.com/2019/02/05/ver1549345599/2341369_810x458.jpg",
+      title:"Kanser Oluşumu nasıl olur? Detaylı bilgi ve ölmeden önce izlenmesi gereken video",
+      explanation:"Kanser hakkında detaylı animasyon video",
+      date:new Date(2022,8,3),
+      tags:["kanser","kanser oluşumu"],
+      username:"mehmetkekec",
+      firstName:"Mehmet",
+      lastName:"Kekeç",
+      profilePhotoUrl:"https://pbs.twimg.com/profile_images/1523976377074163713/hRUFPi6z_400x400.jpg",
+    },
+  ]
+
+const Playlist = () => {
+  return (
+    <div className='playlist-container'>
+        
+    <GridListVideoCard videos={videos} />
+    </div>
+  )
+}
+
+export default Playlist

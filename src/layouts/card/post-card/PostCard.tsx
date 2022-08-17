9cd5e8d8279db2eb39eca63249fbb23490c2ef3b
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-17 22:35:36 +0300

    updated postcard

--- src/layouts/card/post-card/PostCard.tsx
+++ src/layouts/card/post-card/PostCard.tsx
@@ -4,30 +4,24 @@ import PostModel from '../../../models/postModel'
 import "./PostCard.css"
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-interface Props{
-    post:PostModel
+import CardUserInfo from '../../../utilities/components/card-user-info/CardUserInfo';
+interface Props {
+  post: PostModel
 }
-const PostCard:React.FC<Props> = ({post}:Props) => {
+const PostCard: React.FC<Props> = ({ post }: Props) => {
   return (
     <div className='post-card'>
-    <div className='post-card-user-info'>
-        <img alt={post.firstName} className='post-card-user-info-avatar' src={post.profilePhotoUrl} />
-        <div className='post-card-user-info-full-name-username'>
-            <div className='post-card-user-info-full-name'>{selectCharacterHelper( `${post.firstName} ${post.lastName}`,13)} </div>
-            <div className='post-card-user-info-username'>@{post.username}</div>
-        </div>
-        <div className='post-card-user-info-date'>3 hafta önce</div>
+      <CardUserInfo userInfo={{ firstName: post.firstName, lastName: post.lastName, username: post.username, profilePhotoUrl: post.profilePhotoUrl }} />
+      <img alt={post.text} className='post-card-thumbnail-image' src={post.thumbnailUrl} />
+      <div className='post-card-post-text'>{selectCharacterHelper(post.text, 120)}</div>
+      <div className='post-card-operations-icons'>
+        <div style={{ ...(post.postId % 2 === 0 && { color: 'red' }) }}> {post.postId % 2 === 1 ? <FavoriteBorderOutlinedIcon /> : <FavoriteIcon />}</div>
+
+
+
+      </div>
+
     </div>
-    <img alt={post.text} className='post-card-thumbnail-image' src={post.thumbnailUrl} />
-   <div className='post-card-post-text'>{selectCharacterHelper(post.text,120)}</div> 
-   <div className='post-card-operations-icons'>
-   <div style={{...(post.postId % 2 === 0 && {color:'red'})}}> {post.postId % 2 === 1 ?    <FavoriteBorderOutlinedIcon/>  :<FavoriteIcon/>}</div>
-
-  
-
-   </div>
-   
-</div>
   )
 }
 

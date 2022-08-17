fb69462d62d51e52c64fdf16415aa4f08379d22f
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-17 22:45:16 +0300

    Added search history delete button to settings

---
+++ src/pages/settings/Settings.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { deleteLocalStorage } from '../../services/localStorageService';
+import searchHistoryEdit from '../../store/actions/searchHistoryActionCreater';
+import { containLocalStorageKey } from '../../contains/containLocalStorageKey';
+import "./Settings.css";
+
+const Settings = () => {
+
+  const dispatch = useDispatch();
+
+  const { addSearchHistoryState } = bindActionCreators(searchHistoryEdit, dispatch)
+
+  const handleSearchHistoryClear = () => {
+    addSearchHistoryState([])
+    deleteLocalStorage(containLocalStorageKey.SEARCH_HISTORY)
+  }
+  return (
+    <div>
+
+      <button className='settings-delete-search-history-button' onClick={handleSearchHistoryClear}>Arama Geçmişini Temizle</button>
+    </div>
+  )
+}
+
+export default Settings

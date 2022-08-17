94f8696f0ea528b04e73ff08b9381205da3b9b36
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-17 22:40:29 +0300

    updated main because of routes moved to Router

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,16 +1,9 @@
 import React, { useState } from 'react'
 import Navi from '../../layouts/navi/Navi'
-import { Route, Routes } from 'react-router-dom';
-import Home from '../home/Home';
-import Search from '../search/Search';
 import Drawer from '../../layouts/drawer/Drawer';
-import Profile from '../profile/Profile';
 import "./Main.css";
-import Login from '../login/Login';
-import Register from '../register/Register';
-import Payment from '../payment/Payment';
-import LoadMoney from '../load-money/LoadMoney';
-import Settings from '../settings/Settings';
+import Router from '../../routes/Router';
+
 
 const Main = () => {
   const [openDrawer, setOpenDrawer] = useState<boolean>(false);
@@ -21,40 +14,7 @@ const Main = () => {
       <div className={openDrawer ? "grid-container1" : "grid-container2"}>
         <div className='grid-item1'> <Drawer openDrawer={openDrawer} /></div>
         <div className='grid-item2'>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path='/search' element={<Search />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/category/:id' element={<Search />} />
-            <Route path='/payments' element={<Payment />} />
-            <Route path='/buycredit' element={<LoadMoney />} />
-            <Route path='/history' element={<Search />} />
-            <Route path='/readinglist' element={<Search />} />
-            <Route path='/settings' element={<Settings />} />
-            <Route path='/about' element={<Search />} />
-            <Route path='/videochat/:id' element={<Search />} />
-            <Route path='/resetpassword' element={<Search />} />
-            <Route path='/video'>
-              <Route path=':id' element={<Search />} />
-              <Route path='upload' element={<Search />} />
-              <Route path=':id/update' element={<Search />} />
-            </Route>
-            <Route path='/article'>
-              <Route path=':id' element={<Search />} />
-              <Route path='add' element={<Search />} />
-              <Route path=':id/update' element={<Search />} />
-            </Route>
-            <Route path='/chat'>
-              <Route index element={<Search />} />
-              <Route path=':id' element={<Search />} />
-            </Route>
-            <Route path='/profile/:username'>
-              <Route index element={<Profile />} />
-              <Route path='update' element={<Profile />} />
-            </Route>
-            <Route path='/*' element={<Search />} />
-          </Routes>
+        <Router openDrawer={openDrawer} />
         </div>
       </div>
     </div>

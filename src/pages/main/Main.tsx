950e4b49ab30b4f905ecdc88ae689363c47312d8
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-24 13:47:40 +0300

    updated openDrawer for Main page

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,20 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navi from '../../layouts/navi/Navi'
 import Drawer from '../../layouts/drawer/Drawer';
 import "./Main.css";
 import Router from '../../routes/Router';
+import { useSelector } from 'react-redux';
+import State from '../../store/state';
+
 
 
 const Main = () => {
-  const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+
+  const openDrawer = useSelector((state: State) => state.openDrawer.openDrawer)
 
   return (
     <div>
-      <Navi setOpenDrawer={setOpenDrawer} openDrawer={openDrawer} />
+      <Navi/>
       <div className={openDrawer ? "grid-container1" : "grid-container2"}>
-        <div className='grid-item1'> <Drawer openDrawer={openDrawer} /></div>
+        <div className='grid-item1'> <Drawer /></div>
         <div className='grid-item2'>
-        <Router openDrawer={openDrawer} />
+        <Router />
         </div>
       </div>
     </div>

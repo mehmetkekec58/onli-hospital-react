41af66a1971a167b3f0b3e52896717be616cd402
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-24 13:45:01 +0300

    removed unnecesary items

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -9,16 +9,14 @@ const Register = () => {
     return (
         <div className='register-container'>
             <div className='register-form'>
-                <TextField className='register-input-first-name' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.firstName} variant="outlined" color='secondary' />
-                <TextField className='register-input-last-name' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.lastName} variant="outlined" color='secondary' />
-                <TextField className='register-input-username' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.username} variant="outlined" color='secondary' />
-                <TextField className='register-input-email' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.email} variant="outlined" color='secondary' />
-                <TextField className='register-input-password' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.password} variant="outlined" color='secondary' />
-
-                {/* <Link className='i-forgot-my-password' to="/resetpassword">Şifremi Unuttum</Link> */}
-                <button className='register-submit-button' type='submit'>{containTexts.register}</button>
-                <div className='register-already-have-an-account-text'>{containTexts.alreadyHaveAnAccountText}</div>
-                <Link className='register-login-page' to="/login">{containTexts.login}</Link>
+                <TextField className='register-input-first-name' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.FIRST_NAME} variant="outlined" color='secondary' />
+                <TextField className='register-input-last-name' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.LAST_NAME} variant="outlined" color='secondary' />
+                <TextField className='register-input-username' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.USERNAME} variant="outlined" color='secondary' />
+                <TextField className='register-input-email' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.EMAİL} variant="outlined" color='secondary' />
+                <TextField className='register-input-password' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.PASSWORD} variant="outlined" color='secondary' />
+                <button className='register-submit-button' type='submit'>{containTexts.REGISTER}</button>
+                <div className='register-already-have-an-account-text'>{containTexts.ALREADY_HAVE_AN_ACCOUNT_TEXT}</div>
+                <Link className='register-login-page' to="/login">{containTexts.LOGIN}</Link>
             </div>
         </div>
     )

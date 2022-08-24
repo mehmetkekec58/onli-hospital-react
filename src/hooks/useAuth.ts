47984128da998b7ee1d6068cd84ee634e9a149cc
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-24 13:31:55 +0300

    added tokenService and updated authType to useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,8 @@
 import { containUrls } from '../contains/containUrls';
 import { containTexts } from '../contains/containTexts';
-import { containLocalStorageKey } from "../contains/containLocalStorageKey";
-import { getLocalStorage } from "../services/localStorageService";
 import { getRolesService } from "../services/rolesService";
 import AuthType from "./hookTypes/authType";
+import { getToken, tokenValidation } from '../services/tokenService';
 
 
 function checkRoles(roles: string[], getRoles?: string[]): AuthType {
@@ -15,21 +14,25 @@ function checkRoles(roles: string[], getRoles?: string[]): AuthType {
             }
         }
     }
-    return { isAuth: false, message: containTexts.NOT_AUTHORIZED_TO_VIEW_THIS_PAGE, title:containTexts.YOU_ARE_NOT_AUTHORIZED, redirectUrl: containUrls.HOME_PAGE };
+    return { isAuth: false, redirectUrl: containUrls.HOME_PAGE };
 }
 
 export default function useAuth(roles?: string[]): AuthType {
 
-    const token = getLocalStorage(containLocalStorageKey.TOKEN);
-    const getRoles: string[] = getRolesService(token)
+    const token = getToken();
 
-    if (token) {
+
+    if (token && tokenValidation(token)) {
+
+        const getRoles: string[] = getRolesService(token)
+
         if (roles === undefined || roles === null) {
             return { isAuth: true };
         } else {
             return checkRoles(roles, getRoles)
         }
+
     } else {
-        return { isAuth: false, message: containTexts.MUST_BE_LOGGED_IN_TO_VIEW_THIS_PAGE, title: containTexts.LOGIN, redirectUrl: containUrls.LOGIN };
+        return { isAuth: false, redirectUrl: containUrls.LOGIN, alertDialog: { message: containTexts.MUST_BE_LOGGED_IN_TO_VIEW_THIS_PAGE, title: containTexts.LOGIN } };
     }
 }

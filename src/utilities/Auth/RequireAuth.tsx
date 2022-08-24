b1b63a165f01830cbb47ee0dfebda0d7fddab5eb
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-24 13:49:10 +0300

    requireAuth has been developed

--- src/utilities/Auth/RequireAuth.tsx
+++ src/utilities/Auth/RequireAuth.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { containTexts } from "../../contains/containTexts";
 import { containUrls } from "../../contains/containUrls";
 import AlertDialog from "../components/alert-dialog/AlertDialog";
 import useAuth from "../../hooks/useAuth";
+import AuthType from "../../hooks/hookTypes/authType";
 
 
 export default function RequireAuth({ children, roles }: { children: JSX.Element, roles?: string[] }) {
@@ -13,13 +14,25 @@ export default function RequireAuth({ children, roles }: { children: JSX.Element
   const navigate = useNavigate();
 
   const handleClose = () => {
-    navigate(auth.redirectUrl? auth.redirectUrl : containUrls.HOME_PAGE)
+    redirectNavigate(auth.redirectUrl)
     setOpen(false);
   }
 
+  function createAlertDialog(authType: AuthType) {
+    if (authType.alertDialog !== undefined) {
+      return AlertDialog(authType.alertDialog.title ? authType.alertDialog.title : containTexts.WARNING, authType.alertDialog.message ? authType.alertDialog.message : containTexts.OPERATION_FAILED, containTexts.OK, open, handleClose);
+    } else {
+      return <Navigate to={authType.redirectUrl ? authType.redirectUrl : containUrls.HOME_PAGE} />;
+    }
+  }
+
+  function redirectNavigate(url: string | undefined) {
+    navigate(url ? url : containUrls.HOME_PAGE)
+  }
+
   if (!auth.isAuth) {
-    return AlertDialog(auth.title? auth.title :containTexts.WARNING, auth.message? auth.message : containTexts.OPERATION_FAILED, containTexts.OK, open, handleClose);
-  } else{
+    return createAlertDialog(auth)
+  } else {
     return children;
   }
 }

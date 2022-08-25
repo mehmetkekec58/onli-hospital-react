ed7ece2edbc5e9ce5524be6b7586f3e6890ef3a8
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-25 15:10:21 +0300

    updated rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,14 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import activeMenuReducer from "./reducers/activeMenuReducer";
+import loginReducer from "./reducers/loginReducer";
 import openDrawerReducer from "./reducers/openDrawerReducer";
 import searchHistoryReducer from "./reducers/searchHistoryReducer";
 
 const rootReducer = combineReducers({
     searchHistory:searchHistoryReducer,
     openDrawer:openDrawerReducer,
+    activeMenu:activeMenuReducer,
+    login:loginReducer,
     
 })
 export default rootReducer;

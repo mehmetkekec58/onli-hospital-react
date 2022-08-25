234eecfd86f94e926e6ea32925dc5e20411918e0
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-25 15:01:33 +0300

    added containActiveMenus

---
+++ src/contains/containActiveMenus.ts
@@ -0,0 +1,6 @@
+export const containActiveMenus = {
+    NO_LOGIN: [true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true],
+    DOCTOR: [true, false, false, true, false, true, true, true, true, true, true, true, true, true, true, true, true, false],
+    ADMIN: [true, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false],
+    USER: [true, false, false, false, false, true, true, true, false, false, false, true, true, true, true, true, true, false],
+}

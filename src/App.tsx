edfe65a0fb6f234526adae9885d08c377b114756
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-27 11:06:28 +0300

    added snackbar provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Main from './pages/main/Main';
-
+import { SnackbarProvider} from 'notistack';
 function App() {
   return (
     <div className="App">
+      <SnackbarProvider maxSnack={4}>
      <Main/>
+     </SnackbarProvider>
     </div>
   );
 }

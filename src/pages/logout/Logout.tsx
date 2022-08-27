b427e324a63f3fc69c01927a5a173e4b51d25fb4
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-27 11:13:03 +0300

    updated Logout

--- src/pages/logout/Logout.tsx
+++ src/pages/logout/Logout.tsx
@@ -1,22 +1,29 @@
 import { bindActionCreators } from '@reduxjs/toolkit'
+import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { Navigate } from 'react-router-dom'
-import { containUrls } from '../../contains/containUrls'
+import { useNavigate } from 'react-router-dom'
 import { deleteToken } from '../../services/tokenService'
 import LoginFunction from '../../store/actions/loginAction'
 
 
 const Logout = () => {
     const dispatch = useDispatch();
-    const {changeValueLogin} = bindActionCreators(LoginFunction,dispatch)
+    const navigate = useNavigate();
+    const { changeValueLogin } = bindActionCreators(LoginFunction, dispatch)
+
     function SignOut() {
-        deleteToken()
         changeValueLogin(false)
-        return <Navigate to={containUrls.HOME_PAGE} replace />
+        deleteToken()
+        navigate("/")
+
     }
+
+    useEffect(() => {
+        SignOut()
+    }, [])
+
     return (
         <div>
-            {SignOut()}
         </div>
     )
 }

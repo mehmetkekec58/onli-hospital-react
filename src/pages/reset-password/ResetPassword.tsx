0f1c4d694b0e2a7a226272e58e320eb0ed37539f
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-27 11:14:13 +0300

    added reset password

---
+++ src/pages/reset-password/ResetPassword.tsx
@@ -0,0 +1,23 @@
+import TextField from '@mui/material/TextField/TextField'
+import React from 'react'
+import { containTexts } from '../../contains/containTexts'
+import "./ResetPassword.css"
+
+const ResetPassword = () => {
+
+const handleReset = () =>{
+
+}
+
+  return (
+    <div className='reset-password-container'>
+    <div className='reset-password-form'>
+        <div className="reset-password-text">Şifresini sıfırlamak istediğiniz e-posta adresini giriniz.</div>
+        <TextField className='reset-password-input-email' sx={{ width: '50%' }} id="outlined-basic" label={containTexts.EMAİL} variant="outlined" color='secondary' />
+        <button onClick={handleReset} className='reset-password-submit-button' type='submit'>{containTexts.RESET}</button>    
+    </div>
+</div>
+  )
+}
+
+export default ResetPassword

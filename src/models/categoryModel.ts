3539883a83c57ae5bfeeb4f7f5b0aefac4b3a95c
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-27 11:10:59 +0300

    updated catogory and creditPackage model

--- src/models/categoryModel.ts
+++ src/models/categoryModel.ts
@@ -1,6 +1,6 @@
 import IdModel from "./idModel";
 
 export default interface CategoryModel extends IdModel {
-    categoryName: string;
-    categoryPhotoUrl: string;
+    name: string;
+    thumbnailUrl: string;
 }
--- src/models/creditPackageModel.ts
+++ src/models/creditPackageModel.ts
@@ -2,7 +2,8 @@ import IdModel from "./idModel";
 
 export default interface CreditPackageModel extends IdModel {
     creditPackageName: string;
+    photoUrl:string;
     price: number;
-    discount: number;
+    discount: number ;
     creditAmount: number;
 }

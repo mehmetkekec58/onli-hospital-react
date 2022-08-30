d212c54713eb4a545db35c33cfa28d2fc14a1f19
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-30 17:23:05 +0300

    drawer is hidden in small screen sizes

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -5,18 +5,27 @@ import "./Main.css";
 import Router from '../../routes/Router';
 import { useSelector } from 'react-redux';
 import State from '../../store/state';
+import useWindowSize from '../../hooks/useWindowSize';
 
 
 
 const Main = () => {
-
+  const {width} = useWindowSize()
   const openDrawer = useSelector((state: State) => state.openDrawer.openDrawer)
+
+  function drawer(): string {
+    return openDrawer ? "grid-container1" : "grid-container2"
+  }
+  function hideOrShowDrawer() {
+    return (width && width<=606)? "main-normal-container" : drawer();
+   }
+
 
   return (
     <div>
       <Navi/>
-      <div className={openDrawer ? "grid-container1" : "grid-container2"}>
-        <div className='grid-item1'> <Drawer /></div>
+      <div className={hideOrShowDrawer()}>
+      {(width && width>606) &&  <div className='grid-item1'> <Drawer /></div>}
         <div className='grid-item2'>
         <Router />
         </div>

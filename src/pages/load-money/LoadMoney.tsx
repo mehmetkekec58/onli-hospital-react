c1aa9bd71cbd361f8004d890f6551c116abe157f
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-30 17:23:31 +0300

    added exchangeRateCurrency

--- src/pages/load-money/LoadMoney.tsx
+++ src/pages/load-money/LoadMoney.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GridListLoadMoneyCard from '../../layouts/grid-list/grid-list-load-money-card/GridListLoadMoneyCard'
 import CreditPackageModel from '../../models/creditPackageModel'
+import ExchangeRate from '../../services/exchangeRateService'
 import './LoadMoney.css'
 
 const creditPackages: CreditPackageModel[] = [
 {
     id:0,
     photoUrl:"https://cdn.pixabay.com/photo/2014/05/19/18/00/background-348350_960_720.jpg",
-    creditPackageName:"En düşük paket",
+    creditPackageName:"Bozdur bozdur harca",
     price:0.10,
     discount:0,
     creditAmount:1,
@@ -15,7 +16,7 @@ const creditPackages: CreditPackageModel[] = [
 {
     id:4,
     photoUrl:"https://cdn.pixabay.com/photo/2014/05/19/18/00/background-348350_960_720.jpg",
-    creditPackageName:"En düşük paket",
+    creditPackageName:"Al hayrını gör",
     price:1,
     discount:0,
     creditAmount:10,
@@ -23,7 +24,7 @@ const creditPackages: CreditPackageModel[] = [
 {
     id:2,
     photoUrl:"https://cdn.pixabay.com/photo/2014/05/19/18/00/background-348350_960_720.jpg",
-    creditPackageName:"Orta paket paket",
+    creditPackageName:"Orta paket",
     price:5.0,
     discount:0,
     creditAmount:50,
@@ -42,24 +43,32 @@ const creditPackages: CreditPackageModel[] = [
     photoUrl:"https://cdn.pixabay.com/photo/2014/05/19/18/00/background-348350_960_720.jpg",
     creditPackageName:"Güç paketi paket",
     price:100.0,
-    discount:10,
+    discount:5,
     creditAmount:1000,
 },
 {
     id:5,
     photoUrl:"https://cdn.pixabay.com/photo/2014/05/19/18/00/background-348350_960_720.jpg",
-    creditPackageName:"Güç paketi paket",
+    creditPackageName:"Ultra güç paketi",
     price:1000.0,
-    discount:15,
+    discount:10,
     creditAmount:10000,
 },
 ]
 
 const LoadMoney = () => {
-
+    
+    const currency = "TRY"
+    const [exchangeRateCurrency, setExchangeRateCurrency] = useState(0);
+    
+    useEffect(() => {
+        let exchangeRate = new ExchangeRate();
+        exchangeRate.calculateExchangeRate(currency).then((result) => setExchangeRateCurrency(result.data.data));
+      }, [])
+      
     return (
         <div className='load-money-container'>
-           <GridListLoadMoneyCard creditPackages={creditPackages}/> 
+           <GridListLoadMoneyCard currency={currency} exchangeRateCurrency={exchangeRateCurrency}  creditPackages={creditPackages}/> 
         </div>
     )
 }

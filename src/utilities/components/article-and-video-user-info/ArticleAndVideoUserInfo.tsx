8a532718f233c3a13387b1fa5d7dc4932f4283b8
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-31 21:47:59 +0300

    follow button activated

--- src/utilities/components/article-and-video-user-info/ArticleAndVideoUserInfo.tsx
+++ src/utilities/components/article-and-video-user-info/ArticleAndVideoUserInfo.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { containTexts } from '../../../contains/containTexts';
+import { containUrls } from '../../../contains/containUrls';
 import UserInfoModel from '../../../models/userInfoModel';
 import './ArticleAndVideoUserInfo.css';
 
@@ -9,24 +10,29 @@ interface Props {
     userInfo: UserInfoModel
 }
 
-
 const ArticleAndVideoUserInfo: React.FC<Props> = ({ userInfo }: Props) => {
     let navigate = useNavigate();
- //article-and-video-user-info-card-unfollow
-    const handleGoProfilePage = () =>{
-        navigate(`/profile/${userInfo.username}`)
+    const [follower, setFollower] = useState<number>(102);
+    const [follow, setFollow] = useState<boolean>(false);
+
+    const handleFollowOrUnfollow = () => {
+        setFollower(follow ? follower - 1 : follower + 1)
+        setFollow(!follow)
+    }
+    const handleGoProfilePage = () => {
+        navigate(`${containUrls.PROFILE}/${userInfo.username}`)
     }
     return (
         <div className='article-and-video-user-info-container'>
-    
+
             <img onClick={handleGoProfilePage} src={userInfo.profilePhotoUrl} alt={userInfo.firstName} className="article-and-video-user-info-image" />
             <div onClick={handleGoProfilePage} className="article-and-video-user-info-full-name-last-name-username-container">
                 <div className="article-and-video-user-info-first-name-and-last-name">{userInfo.firstName} {userInfo.lastName}</div>
                 <div className="article-and-video-user-info-username">@{userInfo.username}</div>
             </div>
-            <div className="article-and-video-user-follower-count">102 Takipçi</div>
-            <button className="article-and-video-user-info-follow-button ">{containTexts.FOLLOW}</button> 
-        </div >
+            <div className="article-and-video-user-follower-count">{follower} {containTexts.FOLLOWER}</div>
+            <button onClick={handleFollowOrUnfollow} className={follow ? "article-and-video-user-info-follow-button article-and-video-user-info-card-unfollow" : "article-and-video-user-info-follow-button"}>{follow ? containTexts.UNFOLLOW : containTexts.FOLLOW}</button>
+        </div>
 
     )
 }

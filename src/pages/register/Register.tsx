b00f8ff7aca29629592ad13338f221c4f5bd045f
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-08-31 21:50:18 +0300

    updated containText for Register

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -12,7 +12,7 @@ const Register = () => {
             <div className="register-text">{containTexts.REGISTER}</div>
                 <TextField className='register-input-first-name' sx={{ width: '100%' }} id="outlined-basic" label={containTexts.FIRST_NAME} variant="outlined" color='secondary' />
                 <TextField className='register-input-last-name' sx={{ width: '100%' }} id="outlined-basic" label={containTexts.LAST_NAME} variant="outlined" color='secondary' />
-                <TextField className='register-input-email' sx={{ width: '100%' }} id="outlined-basic" label={containTexts.EMAİL} variant="outlined" color='secondary' />
+                <TextField className='register-input-email' sx={{ width: '100%' }} id="outlined-basic" label={containTexts.EMAIL} variant="outlined" color='secondary' />
                 <TextField className='register-input-password' sx={{ width: '100%' }} id="outlined-basic" label={containTexts.PASSWORD} variant="outlined" color='secondary' />
                 <button className='register-submit-button' type='submit'>{containTexts.REGISTER}</button>
                 <div className='register-already-have-an-account-text'>{containTexts.ALREADY_HAVE_AN_ACCOUNT_TEXT}</div>

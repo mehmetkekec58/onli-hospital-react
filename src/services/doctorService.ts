046a78fd0f718674d296a874625ac2538e6245d0
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-09-01 20:10:53 +0300

    added doctorService

---
+++ src/services/doctorService.ts
@@ -0,0 +1,12 @@
+export function getDoctorDetailByUsername(username:string) {
+    
+}
+export function getDoctorInfoByUsername(username:string) {
+    
+}
+export function updateDoctorInfoByUsername(username:string) {
+    
+}
+export function applyForADoctorate(id:number) {
+    
+}

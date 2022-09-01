8ece60798ba0fe11462fe252f513161d88176129
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-09-01 20:11:05 +0300

    added follow service

---
+++ src/services/followService.ts
@@ -0,0 +1,15 @@
+export function follow(id:number) {
+
+}
+
+export function unfollow(id:number) {
+    
+}
+
+export function isFollow(id:number) {
+ return true;    
+}
+
+export function numberOfFollowers(id:number) {
+   return 250; 
+}

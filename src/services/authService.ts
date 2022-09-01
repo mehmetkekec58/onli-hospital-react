8f5320d7c9150fc3ee5da4ba10114a0dbaaf3937
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-09-01 20:10:01 +0300

    added auth service

---
+++ src/services/authService.ts
@@ -0,0 +1,9 @@
+import LoginModel from "../models/loginModel";
+import RegisterModel from "../models/registerModel";
+
+export function login(login:LoginModel) {
+    return true;
+}
+export function register(register:RegisterModel) {
+    return true;
+}

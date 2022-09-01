56ed0bfdad9624110bd49322f84fe4afc5fe51c4
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-09-01 20:10:43 +0300

    added articleService

---
+++ src/services/articleService.ts
@@ -0,0 +1,31 @@
+export function getArticle(id: number) {
+
+}
+export function articleLike(id: number) {
+
+}
+export function removeLike(id: number) {
+
+}
+export function articleAddReadingList(id: number) {
+
+}
+export function articleRemoveReadingList(id: number) {
+
+}
+export function getNumberOfArticleByUsername(username:string) {
+    
+}
+export function getRecommendedArticles(id:number) {
+    
+}
+export function like(id:number) {
+    
+}
+export function articleReadingList(id:number) {
+    
+}
+export function articleAddHistory(id:number) {
+    
+}
+

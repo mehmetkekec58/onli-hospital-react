e09b82d6b55dbacc4b616c0cb273338ad40cc4b4
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-09-09 14:09:53 +0300

    added rTooltip for requireAuthButton

--- src/utilities/Auth/RequireAuthButton.tsx
+++ src/utilities/Auth/RequireAuthButton.tsx
@@ -1,27 +1,28 @@
+import Tooltip from '@mui/material/Tooltip/Tooltip';
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { containTexts } from '../../contains/containTexts';
-import { containUrls } from '../../contains/containUrls';
-import { getToken } from '../../services/tokenService';
+import { constantsText } from '../../constants/constantsText';
+import { constantsUrl } from '../../constants/constantsUrl';
 import State from '../../store/state';
 import alertDialog from '../components/alert-dialog/AlertDialog';
 
 interface Props {
   className?: string | undefined;
+  title?: string;
   onClick?: () => void;
   children?: any;
   errorMessage?: string;
 }
 
-const RequireAuthButton = ({ className, onClick, children, errorMessage }: Props) => {
+const RequireAuthButton = ({ className, onClick, children, errorMessage, title }: Props) => {
   const [open, setOpen] = useState<boolean>(false);
   const login = useSelector((state: State) => state.login.login);
   const navigate = useNavigate();
 
   const handleClose = () => {
     setOpen(false)
-    navigate(containUrls.LOGIN)
+    navigate(constantsUrl.LOGIN)
   }
   const handleCloseIcon = () => {
     setOpen(false)
@@ -33,14 +34,17 @@ const RequireAuthButton = ({ className, onClick, children, errorMessage }: Props
   const auht = login;
 
   if (auht) {
-    return (
+    return (<Tooltip title={title ? title : ""} placement="right">
       <button onClick={onClick} className={className}>{children}</button>
+    </Tooltip>
     )
   } else {
-    return (<div>
-      <button onClick={handleOpenDialog} className={className}>{children}</button>
-      {alertDialog(containTexts.LOGIN, errorMessage ? errorMessage : containTexts.PLEASE_LOGIN, containTexts.OK, open, handleClose, handleCloseIcon)}
-    </div>
+    return (<>
+      <Tooltip title={title ? title : ""} placement="right">
+        <button onClick={handleOpenDialog} className={className}>{children}</button>
+      </Tooltip>
+      {alertDialog(constantsText.LOGIN, errorMessage ? errorMessage : constantsText.PLEASE_LOGIN, constantsText.OK, open, handleClose, handleCloseIcon)}
+    </>
     )
   }
 
